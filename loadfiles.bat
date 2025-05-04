a995af78c6ba3c67c5798444f36a4feafaed16c7
balaju01 <balaju01@gmail.com>
2025-05-04 01:29:38 -0600

    pruebas 04/05/2025

--- loadfiles.bat
+++ loadfiles.bat
@@ -11,28 +11,28 @@ sqlcmd -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte20
 sqlcmd -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -I -Q "TRUNCATE TABLE TMP_ORCHARD;"
 
 :: RENOMBRA ORCHARD
-REN C:\xampp\htdocs\REPORTES_FONARTE\*fullreport*.txt TMP_ORCHARD.txt;
+REN C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\*fullreport*.txt TMP_ORCHARD.txt;
 
 :: RENOMBRA APPLEMUSIC
-REN C:\xampp\htdocs\REPORTES_FONARTE\S1*.txt TMP_APPLEMUSIC.txt;
+REN C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\S1*.txt TMP_APPLEMUSIC.txt;
 
 :: RENOMBRA ITUNES
-REN C:\xampp\htdocs\REPORTES_FONARTE\*_ZZ*.txt TMP_ITUNES.txt;
+REN C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\*_ZZ*.txt TMP_ITUNES.txt;
 
 :: RENOMBRA FINANCIAL_REPORT
-REN C:\xampp\htdocs\REPORTES_FONARTE\financial*.txt TMP_FINANCIAL_REPORT.txt;
+REN C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\financial*.txt TMP_FINANCIAL_REPORT.txt;
 
 :: CARGA REPORTE APPLEMUSIC
-bcp TMP_APPLEMUSIC in C:\xampp\htdocs\REPORTES_FONARTE\TMP_APPLEMUSIC.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
+bcp TMP_APPLEMUSIC in C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\TMP_APPLEMUSIC.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
 
 :: CARGA REPORTE ITUNES
-bcp TMP_ITUNES in C:\xampp\htdocs\REPORTES_FONARTE\TMP_ITUNES.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
+bcp TMP_ITUNES in C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\TMP_ITUNES.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
 
 :: CARGA REPORTE DIVISAS
-bcp TMP_FINANCIAL_REPORT in C:\xampp\htdocs\REPORTES_FONARTE\TMP_FINANCIAL_REPORT.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
+bcp TMP_FINANCIAL_REPORT in C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\TMP_FINANCIAL_REPORT.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
 
 :: CARGA REPORTE ORCHARD
-bcp TMP_ORCHARD in C:\xampp\htdocs\REPORTES_FONARTE\TMP_ORCHARD.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
+bcp TMP_ORCHARD in C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE\TMP_ORCHARD.txt -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -q -c -t \t -r 0x0A -C 65001;
 
 :: ACTUALIZA TABLA APPLEMUSIC
 sqlcmd -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -I -Q "INSERT INTO APPLEMUSIC SELECT * FROM TMP_APPLEMUSIC;"
@@ -47,4 +47,4 @@ sqlcmd -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte20
 sqlcmd -S fonarte2.database.windows.net -d Reporteador -U Dataguys2 -P Fonarte2018 -I -Q "INSERT INTO APPLE_CURRENCY SELECT LEFT(RIGHT(DIVISA,4),3), TIPO_DE_CAMBIO, ANIO, MES FROM TMP_FINANCIAL_REPORT;"
 
 :: MUEVE REPORTES PROCESADOS
-Robocopy C:\xampp\htdocs\REPORTES_FONARTE C:\xampp\htdocs\REPORTES_FONARTE_PROCESADOS /mov /s /w:1 /r:1
+Robocopy C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE C:\xampp\htdocs\SubidaBi\REPORTES_FONARTE_PROCESADOS /mov /s /w:1 /r:1
